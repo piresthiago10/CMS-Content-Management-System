2e2c576d167703b393ac6c41eaea60bd7072f5d6
piresthiago10 <thiagopires.as@gmail.com>
2021-02-22 17:29:36 -0300

    feature - view artigo

--- artigo/views.py
+++ artigo/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from datetime import date
 
-# Create your views here.
+from django.core.exceptions import ViewDoesNotExist
+from django.db.models.query import QuerySet
+from rest_framework import generics, viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from artigo.models import Artigo, Categoria
+from artigo.serializer import ArtigoSerializer, CategoriaSerializer, ListaArtigoPublicadoAtivoSerializer
+
+
+class ArtigoViewSet(viewsets.ModelViewSet):
+    """ Exibindo todos os Artigos"""
+    queryset = Artigo.objects.all()
+    serializer_class = ArtigoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    """ Exibindo todas as categorias """
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class ListaArtigoPublicadoAtivoViewSet(generics.ListAPIView):
+    """ Lista todos os artigos que estão ativos e com data de publicação inferior ao dia atual"""
+
+    def get_queryset(self):
+        data_inicial = "0001-01-01"
+        data_final = date.today()
+        queryset = Artigo.objects.filter(ativo=True).filter(
+            data_publicacao__range=[data_inicial, data_final])
+        return queryset
+    serializer_class = ListaArtigoPublicadoAtivoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
