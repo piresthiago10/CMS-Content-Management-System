586badefe90de6e358983df6b2a589536ed59a57
piresthiago10 <thiagopires.as@gmail.com>
2021-02-22 17:30:43 -0300

    feature - adicionada url de artigos

--- cms/urls.py
+++ cms/urls.py
@@ -1,21 +1,16 @@
-"""cms URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.db import router
+from django.db.models import base
+from django.urls import path, include
+from artigo.views import ArtigoViewSet, CategoriaViewSet, ListaArtigoPublicadoAtivoViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('artigos', ArtigoViewSet, basename='Artigos')
+router.register('categorias', CategoriaViewSet, basename='Categorias')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('artigos/ativo', ListaArtigoPublicadoAtivoViewSet.as_view())
 ]
