73033af057ac5826026cef6b9e8bc6a3469549ed
piresthiago10 <thiagopires.as@gmail.com>
2021-02-22 17:25:19 -0300

    feature - adicionado Artigo e Categoria no admin

--- artigo/admin.py
+++ artigo/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from artigo.models import Artigo, Categoria
 
-# Register your models here.
+
+class Artigos(admin.ModelAdmin):
+    list_display = ('id', 'titulo', 'autor', 'categoria',
+                    'data_publicacao', 'ativo')
+    list_display_links = ('id', 'titulo')
+    search_fields = ('titulo',)
+    list_per_page = 20
+
+
+admin.site.register(Artigo, Artigos)
+
+
+class Categorias(admin.ModelAdmin):
+    list_display = ('id', 'nome')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+
+
+admin.site.register(Categoria, Categorias)
